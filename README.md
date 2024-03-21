ba7787b584934981fb92ef6c48d371a379786abb
Lucas Reali <lucasdevacont@gmail.com>
2024-03-21 09:41:45 -0300

    final-teste02

--- README.md
+++
@@ -1,3 +0,0 @@
-## GAME TRON
-##
-I created a replica of the tron ​​game for a college assignment
--- main.py
+++ main.py
@@ -2,7 +2,48 @@ import pygame
 from pygame.locals import *
 
 def lose():
-    pygame.quit()
+    global game_over
+
+    screen.fill((0, 0, 0))
+
+    font = pygame.font.SysFont(None, 48)
+    lose_text = font.render("YOU LOSE!", True, (255, 0, 0))
+    text_rect = lose_text.get_rect(center=(275, 200))
+    screen.blit(lose_text, text_rect)
+
+    game_over = True
+    button_text = font.render("Press SPACE", True, (255, 255, 225))
+    button_rect = button_text.get_rect(center=(275, 375))
+    screen.blit(button_text, button_rect)
+
+    fontsys = pygame.font.SysFont(None, 25)
+
+    points_text = fontsys.render(f"Blue: {points_player1} | Yellow: {points_player2}", True, (255, 255, 255))
+    points_rect = points_text.get_rect(center=(270, 450))
+    screen.blit(points_text, points_rect)
+
+    pygame.display.update()
+
+    while game_over:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+            elif event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    screen.fill((0, 0, 0))
+                    reset_game()
+                    game_over = False
+
+def reset_game():
+    global player1_direction, player1_pos, player1_direction, player1_trail, player2_direction, player2_pos, player2_trail
+    player1_pos = [275, 600]
+    player1_direction = UP
+    player1_trail = []
+
+    player2_pos = [275, 100]
+    player2_direction = DOWN
+    player2_trail = []
+
 
 pygame.init()
 screen = pygame.display.set_mode((550, 700))
@@ -20,9 +61,6 @@ players_color = (255, 255, 255)
 color_trail1 = (0, 0, 255)
 color_trail2 = (255, 255, 0)
 
-clock_tick = 25
-clock = pygame.time.Clock()
-
 players_speed = 5
 
 player1_pos = [275, 600]
@@ -33,14 +71,19 @@ player2_pos = [275, 100]
 player2_direction = DOWN
 player2_trail = []
 
-running = True
+points_player1 = 0
+points_player2 = 0
+
+clock_tick = 25
+clock = pygame.time.Clock()
+
 
 while True:
     clock.tick(clock_tick)
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            lose()
+            pygame.quit()
 
     keys = pygame.key.get_pressed()
     if keys[K_UP] and player1_direction != DOWN:
@@ -80,15 +123,22 @@ while True:
         player2_pos[0] += players_speed
 
     if player1_pos[0] < 0 or player1_pos[0] >= screen.get_width() or player1_pos[1] < 0 or player1_pos[1] >= screen.get_height():
+        points_player2 += 1
         lose()
 
     if player2_pos[0] < 0 or player2_pos[0] >= screen.get_width() or player2_pos[1] < 0 or player2_pos[1] >= screen.get_height():
+        points_player1 += 1
         lose()
 
     if player1_pos in player1_trail or player1_pos in player2_trail:
+        points_player2 += 1
         lose()
 
     if player2_pos in player1_trail or player2_pos in player2_trail:
+        points_player1 += 1
+        lose()
+
+    if player1_pos == player2_pos:
         lose()
 
     player1_trail.append(player1_pos[:])
